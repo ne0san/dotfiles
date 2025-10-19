50de8c1d63e1342ad6f737fdae58ecff57cbe8ef
neosan <nilva3465@gmail.com>
2025-10-19 21:33:20 +0900

    シェルを呼び出す

--- nix/nix.sh
+++ nix/nix.sh
@@ -5,22 +5,24 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 # Install nix
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 
+# configディレクトリを作成
+mkdir -p ~/.config/nix
+
+# config/nix/nix.confをリンク
+ln -s "${SCRIPT_DIR}/.config/nix/nix.conf" ~/.config/nix/nix.conf
+
 # nix profileでインストールするパッケージ一覧
 packages=(
   "nixpkgs#devenv"
   "nixpkgs#direnv"
   "nixpkgs#nix-direnv"
 )
-
-. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then \
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && \
+  nix --version; \
+fi
 
 # 全部インストール
 for pkg in "${packages[@]}"; do
   nix profile add "$pkg"
 done
-
-# configディレクトリを作成
-mkdir -p ~/.config/nix
-
-# config/nix/nix.confをリンク
-ln -s "${SCRIPT_DIR}/.config/nix/nix.conf" ~/.config/nix/nix.conf
